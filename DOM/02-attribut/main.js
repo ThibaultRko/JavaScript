0ab4592bb62120207dae93ee26d2c03c7b39c402
thibault rko <hercod.thibault@gmail.com>
2023-07-11 14:55:46 +0200

    DOM attribut

---
+++ DOM/02-attribut/main.js
@@ -0,0 +1,7 @@
+const demo = document.getElementById('demo')
+const img = document.getElementById('MyImg')
+const input = document.getElementById('avatarInput')
+
+input.value = img.alt
+
+img.setAttribute("width", 200)
